024c090e6f1d549a604390b4a510e31be5319580
Vani-Gajendra <87956321+Vani-Gajendra@users.noreply.github.com>
2022-03-12 14:58:11 +0800

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InterviewQuestionsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewQuestionsWebApp.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string IntQuestion { get; set; }
+        public string IntAnswer { get; set; }
+
+        public Question() //Constructor
+        {
+
+        }
+    }
+}
